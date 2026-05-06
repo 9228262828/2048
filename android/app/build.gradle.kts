8b1cd572ac6706ee6e7202aa1c4fe8daddb4f9a9
Ahmed Shiref <58848366+9228262828@users.noreply.github.com>
2026-05-06 15:49:12 +0300

    //

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,18 +32,26 @@ android {
         applicationId = "com.game2048.puzzle"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = 1
         versionName = "1.0.0"
     }
-
+    signingConfigs {
+        create("release") {
+            if (keyPropertiesFile.exists()) {
+                keyAlias = keyProperties["keyAlias"] as String
+                keyPassword = keyProperties["keyPassword"] as String
+                storeFile = file(keyProperties["storeFile"] as String)
+                storePassword = keyProperties["storePassword"] as String
+            }
+        }
+    }
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+            signingConfig = signingConfigs.getByName("release")        }
     }
 }
 
